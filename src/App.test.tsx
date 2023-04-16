7e8fe4a05e8467b140690554ce24e23dbed8ddf3
Oleynichenko <ext.aoleynichenko@tcsbank.ru>
2023-04-16 22:46:46 +0300

    fix: tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import { MockedProvider } from '@apollo/client/testing';
 import { screen } from '@testing-library/dom';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Outlet } from 'react-router-dom';
 import { describe, test } from 'vitest';
 import { CardsState } from './components/Cards';
@@ -8,21 +9,25 @@ import { CardPopupHost } from './components/Cards/CardPopup';
 import { Header } from './components/Header/Header';
 import { AlertHost } from './components/UI';
 import { POKEMON_RESPONCES_MOCK } from './mocks';
+import { AppStore } from './store';
 
 describe('App', () => {
   test('Should show header', () => {
     render(
       <>
         <BrowserRouter>
-          <Header></Header>
           <MockedProvider mocks={POKEMON_RESPONCES_MOCK}>
-            <AlertHost>
-              <CardPopupHost>
-                <CardsState>
-                  <Outlet />
-                </CardsState>
-              </CardPopupHost>
-            </AlertHost>
+            <Provider store={AppStore}>
+              <Header></Header>
+
+              <AlertHost>
+                <CardPopupHost>
+                  <CardsState>
+                    <Outlet />
+                  </CardsState>
+                </CardPopupHost>
+              </AlertHost>
+            </Provider>
           </MockedProvider>
         </BrowserRouter>
       </>
--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,9 +1,11 @@
 import { MockedProvider } from '@apollo/client/testing';
 import { screen } from '@testing-library/dom';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { describe, test } from 'vitest';
 import { CardsState } from '../../components/Cards';
 import { AlertHost } from '../../components/UI';
+import { AppStore } from '../../store';
 import { POKEMON_RESPONCES_MOCK } from '../../test-utils';
 import { Home } from './Home';
 
@@ -11,11 +13,13 @@ describe('Home', () => {
   test('Should have cards', async () => {
     render(
       <MockedProvider mocks={POKEMON_RESPONCES_MOCK} addTypename={false}>
-        <AlertHost>
-          <CardsState>
-            <Home></Home>
-          </CardsState>
-        </AlertHost>
+        <Provider store={AppStore}>
+          <AlertHost>
+            <CardsState>
+              <Home></Home>
+            </CardsState>
+          </AlertHost>
+        </Provider>
       </MockedProvider>
     );
 
